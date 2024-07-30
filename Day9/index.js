1575cc060155af5699516802d8d05945bb3ca0e6
Kriti-singh-geeky <kritisingh3297058@gmail.com>
2024-07-30 17:57:24 +0530

    Add files via upload

---
+++ Day9/index.js
@@ -0,0 +1,13 @@
+// Day 9: DOM Manipulation
+
+// Tasks/Activities:
+
+// Activity 1: Selecting and Manipulating Elements
+
+// Task 1: Select an HTML element by its ID and change its text content. 
+
+let ID=document.getElementById("heading").innerText="text content after change";
+
+// Task 2: Select an HTML. element by its class and change its background color.
+
+document.querySelector(".heading").style.backgroundColor='red';
