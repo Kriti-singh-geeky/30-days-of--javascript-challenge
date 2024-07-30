38b7e91c5c6d996ca5dd379b236599dd09aa242a
Kriti-singh-geeky <kritisingh3297058@gmail.com>
2024-07-30 07:37:33 +0530

    Add files via upload

---
+++ Day3.js
@@ -0,0 +1,29 @@
+// Day 3: Control Structures
+
+// Tasks/Activities:
+
+// Activity 1: If-Else Statements
+
+// Task 1: Write a program to check if a number is positive, negative, or zero, and log the result to the console. 
+
+let number1=12;
+if(number1>0){
+    console.log("positive:",number1);
+}
+else if(number1<0){
+    console.log("negative:",number1);
+}
+else{
+    console.log("zero:",number1);
+}
+
+// Task 2: Write a program to check if a person is eligible to vote (age >= 18) and log the result to the console.
+
+let age=14;
+if(age>=18){
+    console.log("eligible:",age);
+}
+else{
+    console.log("not eligible:",age);
+}
+
