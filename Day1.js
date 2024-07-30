e91e01024c335a551d0c62d338a2f401a5fe13ee
Kriti-singh-geeky <kritisingh3297058@gmail.com>
2024-07-30 07:35:34 +0530

    Add files via upload

---
+++ Day1.js
@@ -0,0 +1,68 @@
+// Day 1: Variables and Data Types
+
+// Tasks/Activities:
+
+// Activity 1: Variable Declaration
+
+// Task 1: Declare a variable using var, assign it a number, and log the value to the console.
+
+var num=10;
+console.log('number is=',num);
+
+
+// Task 2: Declare a variable using let, assign it a string, and log the value to the console.
+
+let str="kriti";
+console.log(str);
+
+// Activity 2: Constant Declaration
+
+// Task 3: Declare a variable using const, assign it a boolean value, and log the value to the console.
+
+const vari=true;
+console.log(vari);
+
+// Activity 3: Data Types
+
+// Activity 3: Data Types
+
+//  Task 4: Create variables of different data types (number, string, boolean,   object, array) and log each variable's type using the typeof operator
+
+let number=2;
+console.log(number,typeof(number));
+
+let str1="kriti";
+console.log(str1,typeof(str1));
+
+let isCoding=true;
+console.log(isCoding,typeof(isCoding));
+
+let person={
+    firstName:"kriti",
+    lastName:"singh"
+}
+console.log(typeof(person));
+
+let arr=[1,2,3,4]
+console.log(typeof(arr));
+
+// Activity 4: Reassigning Variables
+
+// Task 5: Declare a variable using let, assign it an initial value, reassign a new value, and log both values to the console
+
+let string="nikhil";
+console.log(string);
+
+string="ram";
+console.log(string);
+
+
+// Activity 5: Understanding const
+
+// Task 6: Try reassigning a variable declared with const and observe the error.
+
+const number1=10;
+console.log(number1);
+
+number1=20;
+console.log(number1);
