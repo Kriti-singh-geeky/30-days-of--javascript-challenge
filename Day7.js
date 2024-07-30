2bb5415e4fc2cc01f2e932afc46c14cee358c325
Kriti-singh-geeky <kritisingh3297058@gmail.com>
2024-07-30 17:56:51 +0530

    Add files via upload

---
+++ Day7.js
@@ -0,0 +1,17 @@
+// Day 7: Objects
+
+// Tasks/Activities:
+
+// Activity 1: Object Creation and Access
+
+// Task 1: Create an object representing a book with properties like title, author, and year, and log the object to the console. 
+
+let book={
+    title:"python",
+    author:"kriti",
+    year:"2020"
+}
+console.log(book);
+
+// Task 2: Access and log the title and author properties of the book object.
+console.log(book.title,book.author);
