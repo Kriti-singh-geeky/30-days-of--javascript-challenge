bf6916594262196a7c44c6e862c55ca93e4cad45
Kriti-singh-geeky <kritisingh3297058@gmail.com>
2024-07-30 07:37:54 +0530

    Add files via upload

---
+++ Day4.js
@@ -0,0 +1,34 @@
+// Day 4: Loops
+
+// Tasks/Activities:
+
+// Activity 1: For Loop
+
+// Task 1: Write a program to print numbers from 1 to 10 using a for loop.
+
+let number=10;
+for(let i=1;i<=number;i++){
+    console.log(i);
+}
+
+// Task 2: Write a program to print the multiplication table of 5 using a for loop.
+
+for(let i=1; i<=10 ;i++){
+    mt=5*i;
+    console.log("5 X" ,i ,"=",mt);
+}
+
+// Activity 2: While Loop
+
+// Task 3: Write a program to calculate the sum of numbers from 1 to 10 using a while loop. 
+
+let i=1;
+let sum=0;
+while (i<=10) {
+    sum=sum+i;
+    i++;
+}
+console.log(sum);
+
+
+
