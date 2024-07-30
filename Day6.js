14836460fd722f7d9fc05bb523ec9bd836d52d9f
Kriti-singh-geeky <kritisingh3297058@gmail.com>
2024-07-30 17:56:11 +0530

    Add files via upload

---
+++ Day6.js
@@ -0,0 +1,39 @@
+// Day 6: Arrays
+
+// Tasks/Activities:
+
+// Activity 1: Array Creation and Access
+
+// Task 1: Create an array of numbers from 1 to 5 and log the array to the console.
+
+let arr=[1,2,3,4,5];
+console.log(arr);
+
+// Task 2: Access the first and last elements of the array and log them to the console.
+
+let array=["apple","mango","lichi"];
+console.log(array[0],array[2]);
+
+// Activity 2: Array Methods (Basic)
+
+// Task 3: Use the push method to add a new number to the end of the array and log the updated array.
+
+let array1=["apple","mango"];
+console.log(array1);
+array1.push("lichi")
+console.log(array1);
+
+// Task 4: Use the pop method to remove the last element from the array and log the updated array.
+
+array1.pop()
+console.log(array1);
+
+// Task 5: Use the shift method to remove the first element from the array and log the updated array.
+
+array1.shift()
+console.log(array1);
+
+// Task 6: Use the unshift method to add a new number to the beginning of the array and log the updated array.
+
+array1.unshift("lichi")
+console.log(array1);
