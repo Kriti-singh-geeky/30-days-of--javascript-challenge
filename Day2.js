49f69a04316461d631437f4365e6ddc02edf290b
Kriti-singh-geeky <kritisingh3297058@gmail.com>
2024-07-30 07:34:57 +0530

    Add files via upload

---
+++ Day2.js
@@ -0,0 +1,74 @@
+// Day 2: Operators
+
+// Tasks/Activities:
+
+// Activity 1: Arithmetic Operations
+
+// Task 1: Write a program to add two numbers and log the result to the console.
+
+let num1=10;
+let num2=20;
+console.log(num1+num2);
+
+// Task 2: Write a program to subtract two numbers and log the result to the console.
+
+let num3=10;
+let num4=20;
+console.log(num3-num4);
+
+// Task 3: Write a program to multiply two numbers and log the result to the console.
+
+let num5=10;
+let num6=20;
+console.log(num5*num6);
+
+// Task 4: Write a program to divide two numbers and log the result to the console.
+
+let num7=10;
+let num8=20;
+console.log(num1/num2);
+
+// Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console
+
+let number2=10;
+let number3=20;
+console.log(number2%number3);
+
+// Activity 2: Assignment Operators
+
+// Task 6: Use the += operator to add a number to a variable and log the result to the console.
+
+let numb=12;
+ numb+=10;
+
+console.log("add=",numb);
+ 
+// Task 7: Use the -= operator to subtract a number from a variable and log the result to the console
+let N=12;
+ N-=10;
+
+console.log(N);
+
+// Activity 3: Comparison Operators
+
+// Task 8: Write a program to compare two numbers using > and < and log the result to the console.
+
+let a=3;
+let b=4;
+console.log("a>b",a>b);
+console.log("a<b",a<b);
+
+//  Task 9: Write a program to compare two numbers using >=
+//    and <= and log the result to the console.
+
+let A=6;
+let B=4;
+console.log("A>=B",A>=B);
+console.log("A<=B",A<=B);
+
+// Task 10: Write a program to compare two numbers using == and === and log the result to the console.
+
+let x=122;
+let string1="122";
+console.log("x==string",x==string1);
+console.log("x===string",x===string1);
