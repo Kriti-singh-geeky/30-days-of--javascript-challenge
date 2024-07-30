783b45a12e72ea7c65d93ad33b36e346c0514ef2
Kriti-singh-geeky <kritisingh3297058@gmail.com>
2024-07-30 17:57:07 +0530

    Add files via upload

---
+++ Day8.js
@@ -0,0 +1,16 @@
+// Day 8: ES6+ Features
+
+// Tasks/Activities:
+
+// Activity 1: Template Literals
+
+// Task 1: Use template literals to create a string that includes variables for a person's name and age, and log the string to the console.
+
+let p_name="kriti";
+let p_age="10";
+console.log(p_name,p_age);
+
+// Task 2: Create a multi-line string using template literals and log it to the console.
+
+multstr="ram is good boy"
+console.log(multstr);
